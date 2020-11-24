04a7ed17f93b839d0208c40024b804c1c2eac2d5
prudiusDenys <prdslife@gmail.com>
2020-11-24 23:31:15 +0100

    end project

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter, HashRouter, Route} from "react-router-dom";
 import { Home } from './UI/components/Home/Home';
 import {MainLayout} from "./UI/common/MainLayout/MainLayout";
 import {News} from "./UI/components/News/News";
 
 export const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
     <div className="App">
       <MainLayout>
         <Route exact path={'/'} render={()=> <Home/>}/>
         <Route exact path={'/news'} render={()=> <News/>}/>
       </MainLayout>
     </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
